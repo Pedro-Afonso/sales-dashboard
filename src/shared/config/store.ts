aa52d0a36d471b2c911fc8e58aa0a218919615a6
Pedro-Afonso <pedro.766@hotmail.com>
2023-03-01 22:25:00 -0300

    feat: Add redux store

--- src/shared/config/store.ts
+++ src/shared/config/store.ts
@@ -1,8 +1,20 @@
-// import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
-// export const setupStore = () => configureStore({})
-export {}
+import { dashboardReducer } from '@/slices/dashboardSlice'
 
-// export type AppStore = ReturnType<typeof store>
-// export type RootState = ReturnType<AppStore['getState']>
-// export type AppDispatch = AppStore['dispatch']
+import { dashboardApi } from '@/services'
+
+const rootReucer = combineReducers({
+  dashboard: dashboardReducer,
+  [dashboardApi.reducerPath]: dashboardApi.reducer
+})
+
+export const setupStore = () =>
+  configureStore({
+    reducer: rootReucer,
+    middleware: gDM => gDM().concat(dashboardApi.middleware)
+  })
+
+export type AppStore = ReturnType<typeof setupStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
