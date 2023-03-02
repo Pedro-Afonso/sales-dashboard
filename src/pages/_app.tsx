92e5d595774425179ea2d967aa9791c54de5d460
Pedro-Afonso <pedro.766@hotmail.com>
2023-03-01 22:25:24 -0300

    feat: Implements the redux store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import { BasicLayout } from '@/shared/layouts/BasicLayout'
 import '@/styles/global.scss'
 import type { AppProps } from 'next/app'
-// import { Provider } from 'react-redux'
-// import { setupStore } from '@/shared/config/store'
+import { Provider } from 'react-redux'
+import { setupStore } from '@/shared/config/store'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    // <Provider store={setupStore()}>
-    <BasicLayout>
-      <Component {...pageProps} />
-    </BasicLayout>
-    // </Provider>
+    <Provider store={setupStore()}>
+      <BasicLayout>
+        <Component {...pageProps} />
+      </BasicLayout>
+    </Provider>
   )
 }
 
